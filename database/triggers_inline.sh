1742d2f50f4730b9633f970845b4b56886d23d9b
Sergio Chaves <sechaves@unal.edu.co>
2026-07-29 21:12:40 -0500

    fix: triggers sin BEGIN/END para evitar problema DELIMITER

--- database/triggers_inline.sh
+++ database/triggers_inline.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-mysql -u root -p$MYSQLPASSWORD unigrades << 'SQLEOF'
-DROP TRIGGER IF EXISTS trg_actualizar_estado_materia_update;
-DROP TRIGGER IF EXISTS trg_actualizar_estado_materia_delete;
-CREATE TRIGGER trg_actualizar_estado_materia_update
-AFTER UPDATE ON nota FOR EACH ROW
-BEGIN
-  DECLARE v INT UNSIGNED;
-  SELECT componente_materia_usuario_id INTO v
-  FROM componente WHERE componente_id = NEW.nota_componente_id;
-  CALL sp_recalcular_estado_materia(v);
-END;
-CREATE TRIGGER trg_actualizar_estado_materia_delete
-AFTER DELETE ON nota FOR EACH ROW
-BEGIN
-  DECLARE v INT UNSIGNED;
-  SELECT componente_materia_usuario_id INTO v
-  FROM componente WHERE componente_id = OLD.nota_componente_id;
-  CALL sp_recalcular_estado_materia(v);
-END;
-SHOW TRIGGERS FROM unigrades LIKE 'trg_actualizar%';
-SQLEOF
+mysql -u root -p$MYSQLPASSWORD unigrades -e "DROP TRIGGER IF EXISTS trg_actualizar_estado_materia_update"
+mysql -u root -p$MYSQLPASSWORD unigrades -e "DROP TRIGGER IF EXISTS trg_actualizar_estado_materia_delete"
+mysql -u root -p$MYSQLPASSWORD unigrades -e "CREATE TRIGGER trg_actualizar_estado_materia_update AFTER UPDATE ON nota FOR EACH ROW CALL sp_recalcular_estado_materia((SELECT componente_materia_usuario_id FROM componente WHERE componente_id=NEW.nota_componente_id))"
+mysql -u root -p$MYSQLPASSWORD unigrades -e "CREATE TRIGGER trg_actualizar_estado_materia_delete AFTER DELETE ON nota FOR EACH ROW CALL sp_recalcular_estado_materia((SELECT componente_materia_usuario_id FROM componente WHERE componente_id=OLD.nota_componente_id))"
+mysql -u root -p$MYSQLPASSWORD unigrades -e "SHOW TRIGGERS FROM unigrades LIKE 'trg_actualizar%'"
